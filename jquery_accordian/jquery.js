d31550f19b3c8a4a9ff4d5c4934ee3de0551d48d
russell <russelltaylor05@gmail.com>
2013-08-08 16:49:51 -0700

    jquery starter edits

--- jquery_accordian/jquery.js
+++ jquery_accordian/jquery.js
@@ -1,17 +1,18 @@
-
 $(document).ready(function(){
-	
-$("#cssmenu > ul > li > a ").click(function(){
- 
-    if(false == $(this).next().is(':visible')) {
-        $('#cssmenu ul ul').slideUp(300);
-    }
-    $(this).next().slideToggle(300);
-});
- 
-$('#cssmenu > ul > li:eq(0)').show();
-
-$('#cssmenu').hide();
-
-
+	$('#cssmenu ul ul li:odd').addClass('odd');
+	$('#cssmenu ul ul li:even').addClass('even');
+	$('#cssmenu > ul > li > a').click(function() {
+		$('#cssmenu li').removeClass('active');
+		$(this).closest("li").addClass('active');
+		
+		var checkElement = $(this).next();
+		if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+			return false;
+		}
+		if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+			$('#cssmenu ul ul:visible').slideUp('normal');
+			checkElement.slideDown('normal');
+			return false;
+		}
+	});
 });
